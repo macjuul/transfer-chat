0359e3de1566e6cc76f2690fee780f2de951e985
macjuul <minecraftmacjuul@gmail.com>
2017-02-15 09:40:42 +0100

    Preparing for nio update 2.1

---
+++ Shared/src/main/java/net/exodiusmc/platformer/shared/nio/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * A packet networking built on Netty, allowing multiple instances
+ * to communicate with each other
+ *
+ * @author Macjuul
+ * @version 2.1.0
+ * @since 15/02/2017
+ */
+package net.exodiusmc.platformer.shared.nio;
